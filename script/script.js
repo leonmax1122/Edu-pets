62897e00d994c4ef613c282e7df894929d8efa98
Dukeeeomg <samuduc2008@gmail.com>
2026-04-16 15:53:08 -0500

    lol

--- script/script.js
+++ script/script.js
@@ -1,14 +1,3 @@
-const canvas = document.getElementById('symbols');
-const ctx = canvas.getContext('2d');
-
-function resizeCanvas() {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-}
-
-resizeCanvas();
-window.addEventListener('resize', resizeCanvas);
-
 const symbols = ['π','√','∞','∑','∆','≠','≈','∫','∂'];
 const particles = [];
 
